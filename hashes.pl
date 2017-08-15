ce624911d864fad11c9a35b3a38fa0fb246f4955
Swapnil Sawdekar <swapnil.sawdekar@gmail.com>
2017-08-15 21:33:42 +0530

    Add array of hash
    Added example for an array of hashes.

--- hashes.pl
+++ hashes.pl
@@ -1,7 +1,8 @@
 sub typesOfHashes {
+    print ("Simple hash\n");
     my %basicHash = ("mon", "Monday", "tue", "Tuesday", "wed", "Wednesday");   #Create simple hash
     foreach my $key (sort keys %{basicHash}) {        #Get each key in sorted order
-        print ("${key} => ${basicHash{$key}}\n");     #Print key and value of hash
+        print ("\t${key} => ${basicHash{$key}}\n");     #Print key and value of hash
     }
 ##################################################################################################
     print ("Alternate Way of storing data into hash using => operator\n");
@@ -12,7 +13,7 @@ sub typesOfHashes {
         "wed" => "Wednesday",
     );
     foreach my $key (sort keys %{basicHash1}) {        #Get each key in sorted order
-        print ("${key} => ${basicHash1{$key}}\n");     #Print key and value of hash
+        print ("\t${key} => ${basicHash1{$key}}\n");     #Print key and value of hash
     }
 
 ##################################################################################################
@@ -22,8 +23,46 @@ sub typesOfHashes {
     $basicHash2{"tue"} = "Tuesday";
     $basicHash2{"wed"} = "Wednesday";
     foreach my $key (sort keys %{basicHash2}) {        #Get each key in sorted order
-        print ("${key} => ${basicHash2{$key}}\n");     #Print key and value of hash
+        print ("\t${key} => ${basicHash2{$key}}\n");     #Print key and value of hash
     }
 
 ##################################################################################################
 }
+
+sub arrayOfHash {
+    my @AoH = (                                       #Each element of array is hash
+        {
+           husband  => "barney",
+           wife     => "betty",
+           son      => "bamm bamm",
+        },
+        {
+           husband => "george",
+           wife    => "jane",
+           son     => "elroy",
+        },
+
+        {
+           husband => "homer",
+           wife    => "marge",
+           son     => "bart",
+        },
+    );
+    push @AoH, {husband => "fred", wife => "Wilma", son => "Jenny"};    #Push one more hash into array
+#    print $AoH[0]{"husband"};
+#     foreach my $currentHash (@AoH) {
+#         foreach my $key (keys %{currentHash}) {
+#             print("${key}\n");
+#         }
+#     }
+    foreach my $i (0 .. (scalar @AoH)-1) {                             #Traverse array (scalar function returns number of elements in the array
+        print ("Array[${i}]....\n");
+        foreach my $key (sort keys %{$AoH[$i]}) {                      #$AoH[$i] is each hash
+            print ("\t$key => $AoH[$i]->{$key}\n");                    #Print each value of hash
+        }
+    }
+}
+
+typesOfHashes();    #Basics of hash
+print ("##################################################################################################\n");
+arrayOfHash();      #Array of hashes
