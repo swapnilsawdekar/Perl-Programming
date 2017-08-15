b9f1a8c3ff4f6d7de61c1a3d03560f83249e3878
Swapnil Sawdekar <swapnil.sawdekar@gmail.com>
2017-08-15 20:28:59 +0530

    Update hashes.pl
    Add more ways to store data into hash

--- hashes.pl
+++ hashes.pl
@@ -3,4 +3,27 @@ sub typesOfHashes {
     foreach my $key (sort keys %{basicHash}) {        #Get each key in sorted order
         print ("${key} => ${basicHash{$key}}\n");     #Print key and value of hash
     }
+##################################################################################################
+    print ("Alternate Way of storing data into hash using => operator\n");
+    my %basicHash1;
+    %basicHash1 = (                               #Store key and value pair using => operator
+        "mon" => "Monday",
+        "tue" => "Tuesday",
+        "wed" => "Wednesday",
+    );
+    foreach my $key (sort keys %{basicHash1}) {        #Get each key in sorted order
+        print ("${key} => ${basicHash1{$key}}\n");     #Print key and value of hash
+    }
+
+##################################################################################################
+    print ("One more way to store data into hash using scalar method\n");
+    my %basicHash2;
+    $basicHash2{"mon"} = "Monday";
+    $basicHash2{"tue"} = "Tuesday";
+    $basicHash2{"wed"} = "Wednesday";
+    foreach my $key (sort keys %{basicHash2}) {        #Get each key in sorted order
+        print ("${key} => ${basicHash2{$key}}\n");     #Print key and value of hash
+    }
+
+##################################################################################################
 }
