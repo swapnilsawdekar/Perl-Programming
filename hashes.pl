092c6a6d5dd89016095f9d1d0fbcccfcde27e7bb
Swapnil Sawdekar <swapnil.sawdekar@gmail.com>
2017-08-21 10:11:25 +0530

    Update print statements by say()
    say is introduced in perl v5.14 and it is same as print except addition of newline

--- hashes.pl
+++ hashes.pl
@@ -1,11 +1,12 @@
+use v5.14;
 sub typesOfHashes {
-    print ("Simple hash\n");
+    say ("Simple hash");
     my %basicHash = ("mon", "Monday", "tue", "Tuesday", "wed", "Wednesday");   #Create simple hash
     foreach my $key (sort keys %{basicHash}) {        #Get each key in sorted order
-        print ("\t${key} => ${basicHash{$key}}\n");     #Print key and value of hash
+        say ("\t${key} => ${basicHash{$key}}");     #Print key and value of hash
     }
 ##################################################################################################
-    print ("Alternate Way of storing data into hash using => operator\n");
+    say ("Alternate Way of storing data into hash using => operator");
     my %basicHash1;
     %basicHash1 = (                               #Store key and value pair using => operator
         "mon" => "Monday",
@@ -13,17 +14,17 @@ sub typesOfHashes {
         "wed" => "Wednesday",
     );
     foreach my $key (sort keys %{basicHash1}) {        #Get each key in sorted order
-        print ("\t${key} => ${basicHash1{$key}}\n");     #Print key and value of hash
+        say ("\t${key} => ${basicHash1{$key}}");     #Print key and value of hash
     }
 
 ##################################################################################################
-    print ("One more way to store data into hash using scalar method\n");
+    say ("One more way to store data into hash using scalar method");
     my %basicHash2;
     $basicHash2{"mon"} = "Monday";
     $basicHash2{"tue"} = "Tuesday";
     $basicHash2{"wed"} = "Wednesday";
     foreach my $key (sort keys %{basicHash2}) {        #Get each key in sorted order
-        print ("\t${key} => ${basicHash2{$key}}\n");     #Print key and value of hash
+        say ("\t${key} => ${basicHash2{$key}}");     #Print key and value of hash
     }
 
 ##################################################################################################
@@ -49,28 +50,28 @@ sub arrayOfHash {
         },
     );
     push @AoH, {husband => "fred", wife => "Wilma", son => "Jenny"};    #Push one more hash into array
-    print $AoH[0]{"husband"}."\n";                                           #Print specific element from array of hash
-    print ("Display contents of array of hash by using De-Referencing on each pointer element of array\n");
+    say $AoH[0]{"husband"}."";                                           #Print specific element from array of hash
+    say ("Display contents of array of hash by using De-Referencing on each pointer element of array");
     my $index = 0;
     foreach my $currentHash (@AoH) {
-        print ("Array[${index}]....\n");
+        say ("Array[${index}]....");
         foreach my $key (sort keys %${currentHash}) {                   #De-Referencing the pointer of hash
             my %h = %${currentHash};                                    #De_Referencing hash from pointer
-            print("\t${key} => $h{$key}\n");
-            print("\t${key} => ${$currentHash}{$key}\n");                 #De-Referencing hash from pointer
+            say("\t${key} => $h{$key}");
+            say("\t${key} => ${$currentHash}{$key}");                 #De-Referencing hash from pointer
         }
         $index++;
     }
 ##################################################################################################
-    print ("Display contents of array of hash using array indexing\n");
+    say ("Display contents of array of hash using array indexing");
     foreach my $i (0 .. (scalar @AoH)-1) {                             #Traverse array (scalar function returns number of elements in the array
-        print ("Array[${i}]....\n");
+        say ("Array[${i}]....");
         foreach my $key (sort keys %{$AoH[$i]}) {                      #$AoH[$i] is each hash
-            print ("\t$key => $AoH[$i]->{$key}\n");                    #Print each value of hash
+            say ("\t$key => $AoH[$i]->{$key}");                    #Print each value of hash
         }
     }
 }
 
 typesOfHashes();    #Basics of hash
-print ("##################################################################################################\n");
+say ("##################################################################################################");
 arrayOfHash();      #Array of hashes
